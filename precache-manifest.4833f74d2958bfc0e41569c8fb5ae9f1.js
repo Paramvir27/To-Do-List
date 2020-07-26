008353efaf29fccdeec3d10c4c41be2aab1b0969
Paramvir Singh <48600406+Paramvir27@users.noreply.github.com>
2020-07-26 13:35:46 +0530

    Add files via upload

---
+++ precache-manifest.4833f74d2958bfc0e41569c8fb5ae9f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f96c5e18dbb338fc7ac67c603fa5978",
+    "url": "./index.html"
+  },
+  {
+    "revision": "788b85e1ef5941f80aaf",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "68061e809e935057bbcd",
+    "url": "./static/css/main.6a1f823f.chunk.css"
+  },
+  {
+    "revision": "788b85e1ef5941f80aaf",
+    "url": "./static/js/2.b98efac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b98efac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68061e809e935057bbcd",
+    "url": "./static/js/main.1dc0bb30.chunk.js"
+  },
+  {
+    "revision": "7888f9ce2489bc40833b",
+    "url": "./static/js/runtime-main.ada85091.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
